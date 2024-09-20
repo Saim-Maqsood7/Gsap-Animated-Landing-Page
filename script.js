b874de3bb1d96e7b3a93761bad515663ef9147cb
Saim Maqsood <your-email@example.com>
2024-09-20 17:01:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+gsap.from("#img1",{
+    delay : 0.4,
+    opacity: 0,
+    duration: 1,
+    y:80
+    
+})
+gsap.from('#img2',{
+    opacity:0,
+    delay: 0.4,
+    duration:1,
+    x:100
+
+
+})
+gsap.from("#img3",{
+    delay : 0.4,
+    opacity: 0,
+    duration: 1,
+    y:-80
+    
+})
